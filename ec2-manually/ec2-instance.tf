1e46ab72d3c1e050dd7ce3822fa6b0d9f8fc96d3
SAJIL.PEREPPADANBABY <T0125M8@stellantis.com>
2024-11-10 14:56:36 +0530

    modifications

--- ec2-manually/ec2-instance.tf
+++ ec2-manually/ec2-instance.tf
@@ -2,10 +2,8 @@ resource "aws_instance" "myinstance" {
    
   instance_type = var.aws_instance_type
   ami = data.aws_ami.amzlinux2.id
-  subnet_id = aws_vpc.main.id
+  subnet_id = aws_subnet.public.id
 
-  depends_on = [ aws_vpc.main.id ]
-  
   tags ={
     name = "ec2"
   }
