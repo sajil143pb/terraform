c785cce3cc1e05994a1e399d96009cc540731d40
SAJIL.PEREPPADANBABY <T0125M8@stellantis.com>
2024-11-10 14:46:28 +0530

    ec2 instance

--- ec2-manually/ec2-instance.tf
+++ ec2-manually/ec2-instance.tf
@@ -1,5 +1,9 @@
 resource "aws_instance" "myinstance" {
+   
   instance_type = var.aws_instance_type
   ami = data.aws_ami.amzlinux2.id
-  
+  subnet_id = aws_vpc.main.id
+  tags ={
+    name = "ec2"
+  }
 }
